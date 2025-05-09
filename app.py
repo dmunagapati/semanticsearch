1695bd62324b377d97f9827df494a443a4ff8d24
dmunagapati <dmunagapati@gmail.com>
2025-05-08 22:20:44 -0400

    changed binding address for render requirement

--- app.py
+++ app.py
@@ -63,4 +63,6 @@ def search_relevant_chunks(query, chunks, embeddings):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
